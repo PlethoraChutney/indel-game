089653216fceb998eebf6ce5b98673b28811d813
Rich <rich@posert.com>
2022-01-19 22:46:00 -0800

    Use blazing fast levenshtein to remove spaces

--- indel.py
+++ indel.py
@@ -5,55 +5,43 @@ import logging
 import numpy as np
 import json
 import random
+import Levenshtein
+
 from datetime import datetime
 
 with open('words.json', 'r') as f:
     word_list = json.load(f)
 
+
 def generate_word_pair():
+    while True:
+        try:
+            starting_word = random.choice(word_list)
+            prev_words = [starting_word]
+            for _ in range(random.randrange(15, 21)):
+                print('Iterating...')
+                word_len = len(prev_words[-1])
+                working_list = [x for x in word_list if any([len(x) == word_len-1, len(x) == word_len, len(x) == word_len + 1])]
+                good_list = [x for x in working_list if Levenshtein.distance(x, prev_words[-1]) == 1 and x not in prev_words]
+                print(good_list)
+                prev_words.append(
+                    random.choice(good_list)
+                )
+            break
+        except IndexError:
+            continue
+
     words_and_time = [
-        random.choice(word_list),
-        random.choice(word_list),
+        starting_word,
+        prev_words[-1],
         datetime.now().day
     ]
 
-    if words_and_time[0] == words_and_time[1]:
-        words_and_time = generate_word_pair()
+    return words_and_time, prev_words
 
-    return words_and_time
-
-words_and_time = generate_word_pair()
+words_and_time, word_path = generate_word_pair()
 
 checker = spellchecker.SpellChecker()
-
-def levenshtein_distance(token1, token2):
-    # https://blog.paperspace.com/measuring-text-similarity-using-levenshtein-distance/
-    # if this is slow it could definitely be vectorized
-    #
-    # it's quite fast
-
-    token1 = token1.replace(' ', '')
-    token2 = token2.replace(' ', '')
-    
-    distances = np.zeros((len(token1) + 1, len(token2) + 1))
-    distances[:,0] = np.arange(len(token1) + 1)
-    distances[0] = np.arange(len(token2) + 1)
-
-    for ind_one in range(len(token1)):
-        for ind_two in range(len(token2)):
-            # remember we're currently working on index + 1
-            # because row/col 0 is initialized with the range
-            if token1[ind_one] == token2[ind_two]:
-                distances[ind_one + 1, ind_two + 1] = distances[ind_one, ind_two]
-            else:
-                distances[ind_one + 1, ind_two + 1] = np.min([
-                    distances[ind_one, ind_two],
-                    distances[ind_one + 1, ind_two],
-                    distances[ind_one, ind_two + 1]
-                ]) + 1
-
-    # distance between tokens is bottom-right entry in matrix
-    return distances[len(token1), len(token2)]
 
 
 app = Flask(__name__)
@@ -64,13 +52,11 @@ except KeyError:
     app.secret_key = 'BAD_SECRET_KEY_DEV_ONLY'
 
 def check_word(word, prev_word):
-    words = [x.strip() for x in word.split(' ') if x]
-    spellcheck_answers = [len(checker.unknown([x])) == 0 for x in words]
-    word_list_answers = [x in word_list for x in words]
-    complete_answers = [spellcheck_answers[i] or word_list_answers[i] for i in range(len(words))]
+    spellcheck_answers = len(checker.unknown([word])) == 0
+    word_list_answers = word in word_list
     return {
-        'word': all(complete_answers),
-        'distance': bool(levenshtein_distance(word, prev_word) <= 1)
+        'word': spellcheck_answers or word_list_answers,
+        'distance': bool(Levenshtein.distance(word, prev_word) <= 1)
     }
 
 
@@ -78,7 +64,7 @@ def check_word(word, prev_word):
 def index():
     if request.method == 'GET':
         if datetime.now().day != words_and_time[2]:
-            words_and_time[0:2] = generate_word_pair()
+            words_and_time[0:2], path = generate_word_pair()
 
         return render_template('indel.html', night_theme = request.args.get('theme') == 'dark')
 
